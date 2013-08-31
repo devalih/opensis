cdbd68609ed2f3905cc01050b9a4dd0e32c3ea86
Ali <devalih@live.com>
2013-08-31 03:31:00 -0700

    add data file

---
+++ data.php
@@ -0,0 +1,8 @@
+<?php 
+$DatabaseType = 'mysql'; 
+$DatabaseServer = '127.0.0.1'; 
+$DatabaseUsername = 'root'; 
+$DatabasePassword = '123qweasdd'; 
+$DatabaseName = 'opensis'; 
+$DatabasePort = '3306'; 
+?>
