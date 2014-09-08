01ab3df76da93d880d468903a57b7b4fd89b24a2
yazanho <yazanho_89@hotmail.com>
2014-09-09 00:36:55 +0400

    Update General_Info.inc.php
    remove ethintical from genral info

--- modules/Students/includes/General_Info.inc.php
+++ modules/Students/includes/General_Info.inc.php
@@ -268,8 +268,8 @@ else
 echo'</td></tr>';
 echo '<tr><td>Estimated Grad. Date </td><td>:</td><td>'.DateInput($student['ESTIMATED_GRAD_DATE'],'students[ESTIMATED_GRAD_DATE]','').'</td></tr>';
 echo '<tr><td>Gender<font color=red>*</font></td><td>:</td><td>'.SelectInput($student['GENDER'],'students[GENDER]','',array('Male'=>'Male','Female'=>'Female'),'N/A','').'</td></tr>';
-echo '<tr><td>Ethnicity<font color=red>*</font></td><td>:</td><td>'.SelectInput($student['ETHNICITY'],'students[ETHNICITY]','',$ethnic_option,'N/A','').'</td></tr>';
-echo '<tr><td>Common Name</td><td>:</td><td>'.TextInput($student['COMMON_NAME'],'students[COMMON_NAME]','','size=10 class=cell_medium maxlength=10').'</td></tr>';
+
+
 echo '<tr><td>Birthdate<font color=red>*</font></td><td>:</td><td>'.DateInput($student['BIRTHDATE'],'students[BIRTHDATE]','').'</td></tr>';
 echo '<tr><td>Primary Language</td><td>:</td><td>'.SelectInput($student['LANGUAGE'],'students[LANGUAGE]','',$language_option,'N/A','').'</td></tr>';
 
